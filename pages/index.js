ff81dcc69f9f614a51b7644e622c7058b11ba942
Pedro Dec <pcamposdec@gmail.com>
2026-02-25 12:23:10 +0000

    feat: add initial page

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div>
+      <h1>EcoSort AI</h1>
+      <h2>
+        Trabalho de conclusão de curso dos alunos do Instituto Mauá de
+        Tecnologia: Lixeira inteligente com separação automática de resíduos com
+        o uso de inteligência artificial.
+      </h2>
+    </div>
+  );
+}
+
+export default Home;

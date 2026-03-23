cbbe18c6d54e1de30000cbe4cef83ceb0b366d52
Pedro Dec <pcamposdec@gmail.com>
2026-03-23 16:25:21 +0000

    feat: create `trashEvent` model and `/api/v1/trash-events` endpoint

---
+++ models/trashEvent.js
@@ -0,0 +1,39 @@
+import database from "infra/database.js";
+
+async function create(eventData) {
+  const newEvent = await runInsertQuery(eventData);
+  return newEvent;
+
+  async function runInsertQuery(data) {
+    const results = await database.query({
+      text: `
+        INSERT INTO trash_detections 
+          (bin_id, item_class, confidence, detected_at) 
+        VALUES 
+          ($1, $2, $3, $4) 
+        RETURNING *;
+      `,
+      values: [
+        data.bin_id,
+        data.detection.class_name,
+        data.detection.confidence,
+        data.timestamp,
+      ],
+    });
+    return results.rows[0];
+  }
+}
+
+async function listEvents() {
+  const results = await database.query(
+    "SELECT * FROM trash_detections ORDER BY detected_at DESC LIMIT 10;",
+  );
+  return results.rows;
+}
+
+const trashEvent = {
+  create,
+  listEvents,
+};
+
+export default trashEvent;

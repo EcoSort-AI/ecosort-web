e1eca29d961c28953535245bfd2b6757b5152dd2
Pedro Dec <pcamposdec@gmail.com>
2026-07-10 10:56:48 +0000

    refactor: fixes indentation and whitespace according to the linter

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-// Importa o arquivo CSS global que acabamos de criar
+/* eslint-disable no-unused-vars */
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {

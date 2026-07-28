9f991aba5e1778b55aaf084200f675b2e28cf874
Pedro Campos Dec <pcamposdec@gmail.com>
2026-07-28 14:13:18 -0300

    feat: implement `pending` toast and notifications in side-bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import "../styles/globals.css";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from 'sonner';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <TooltipProvider>
       <Component {...pageProps} />
+      <Toaster theme="dark" position="top-right" richColors />
     </TooltipProvider>
   );
 }
